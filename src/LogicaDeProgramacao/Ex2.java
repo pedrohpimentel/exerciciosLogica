f87d0f472e5272ac07320f192fa47df368f642e7
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2025-09-23 06:46:41 -0300

    Verificando se o número é par ou ímpar

---
+++ src/LogicaDeProgramacao/Ex2.java
@@ -0,0 +1,25 @@
+package LogicaDeProgramacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex2 {public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+        /* Escreva um programa que solicite ao usuário que digite um número inteiro.
+        Verifique se o número é par ou ímpar.
+        Imprima a mensagem "O número é par" ou "O número é ímpar", de acordo com o resultado.*/
+
+    System.out.print("Digite um valor inteiro: ");
+    int num = sc.nextInt();
+
+    if (num % 2 == 0){
+        System.out.println("O número é par.");
+    } else {
+        System.out.println("O número é ímpar.");
+    }
+
+    sc.close();
+}
+}
